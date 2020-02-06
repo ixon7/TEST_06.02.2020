9300eb1c857b49da2f97576b60768230209a97e6
ixon7 <55660649+ixon7@users.noreply.github.com>
2020-02-06 13:29:08 +0100

    Dodaj pliki projektów.

---
+++ TEST_06.02.2020/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TEST_06._02._2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
