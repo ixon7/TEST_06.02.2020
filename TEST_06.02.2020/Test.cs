725510fe2a08194be9051f93a04a2812208b0c26
ixon7 <55660649+ixon7@users.noreply.github.com>
2020-02-06 15:23:07 +0100

    piaty

---
+++ TEST_06.02.2020/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TEST_06._02._2020
+{
+    class Test
+    {
+        public int ID { get; set; }
+        public string Nazwa { get; set; }
+        public string Opis { get; set; }
+        public bool Powodzenie { get; set; }
+        public string Tester { get; set; }
+
+        ~Test()
+        {
+
+        }
+    }
+}
